3ec600b922b65e6f8133ee0ebb0ef7b8169e55d8
Ryan <32682207+rzwier2@users.noreply.github.com>
2020-05-28 16:24:30 -0400

    Update AVG_BallsInPlay.sql

--- AVG_BallsInPlay.sql
+++ AVG_BallsInPlay.sql
@@ -1,6 +1,6 @@
 use baseballstats;
 
-SELECT HitterID, PlayerNum AS 'Number', FirstName AS 'First Name', LastName AS 'Last Name', H, HR, AtBats, (H - HR) / (AtBats - HR) AS 'AVG on Balls in Play'
+SELECT HitterID, PlayerNum AS 'Number', FirstName AS 'First Name', LastName AS 'Last Name', H AS 'Hits', HR, AtBats, (H - HR) / (AtBats - HR) AS 'AVG on Balls in Play'
 FROM hitter INNER JOIN player ON hitter.PlayerID = player.PlayerID
 WHERE AtBats > 20
 GROUP BY HitterID
